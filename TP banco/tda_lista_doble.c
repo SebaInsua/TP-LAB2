b35630c4d9be293352ff183b2b5367a07bb48093
SebaInsua <sebastianinsua23@gmail.com>
2022-09-15 18:50:13 -0300

    added funct ld

--- TP banco/tda_lista_doble.c
+++ TP banco/tda_lista_doble.c
@@ -21,12 +21,107 @@ void mostrar_caja(Caja aux){
     printf("Numero de caja: \n",aux.numero_caja);
 }
 ///PROTOTIPADO TDA lista-doble
-nodoDoble* crearNodoDoble       (Cliente aux);
-nodoDoble* agregarAlPpioDoble   (nodoDoble*lista,nodoDoble*nuevo_nodo);
-nodoDoble* agregarAlFinalDoble  (nodoDoble*lista,nodoDoble*nuevo_nodo);
-nodoDoble* buscarUltimoDoble    (nodoDoble*lista);
-nodoDoble* agregarOrdenDoble    (nodoDoble*lista,nodoDoble*nuevo_nodo);
-nodoDoble* eliminarNodoDoble    (nodoDoble*lista,char nombre_borrar[]);
-void mostrarNodo                (Cliente aux);
-void mostrarListaDoble          (nodoDoble* lista);
+nodoDoble* crearNodoDoble       (Cliente aux){
+    nodoDoble*nuevo_nodo=(nodoDoble*) malloc (sizeof(nodoDoble));
+    nuevo_nodo->cliente=aux;
+    nuevo_nodo->anterior=NULL;
+    nuevo_nodo->siguiente=NULL;
+    return nuevo_nodo;
+}
+nodoDoble* agregarAlPpioDoble   (nodoDoble*lista,nodoDoble*nuevo_nodo){
+    if(lista){
+        nuevo_nodo->siguiente=lista;
+        if(lista->siguiente){
+            lista->anterior=lista;
+        }
+    }
+    lista=nuevo_nodo;
+    return lista;
+}
+nodoDoble* agregarAlFinalDoble  (nodoDoble*lista,nodoDoble*nuevo_nodo){
+    if(lista){
+        nodoDoble*ultimo=buscarUltimoDoble(lista);
+        ultimo->siguiente=nuevo_nodo;
+        nuevo_nodo->anterior=ultimo;
+    }else{
+        lista=nuevo_nodo;
+    }
+    return lista;
+}
+nodoDoble* buscarUltimoDoble    (nodoDoble*lista){
+    if(lista){
+       nodoDoble*ultimo=lista;
+       while(ultimo){
+        ultimo=ultimo->siguiente;
+       }
+    }
+    return ultimo;
+}
+nodoDoble* agregarOrdenDoble    (nodoDoble*lista,nodoDoble*nuevo_nodo){
+    if(lista){
+        nodoDoble*aux=lista;
+        if(strcmpi(aux->cliente.nombre,nuevo_nodo->cliente.nombre)>0){
+            lista=agregarAlPpioDoble(lista,nuevo_nodo);
+        }else{
+            nodoDoble*ante=lista;
+            nodoDoble*seg=lista->siguiente;
+            while(seg && strcmpi(aux->cliente.nombre,nuevo_nodo->cliente.nombre)<0){
+                ante=seg;
+                seg=seg->siguiente;
+            }
+            if(seg){
+                ante->siguiente=nuevo_nodo;
+                nuevo_nodo->siguiente=seg;
+                nuevo_nodo->anterior=ante;
+                seg->anterior=nuevo_nodo;
+            }else{
+                lista=agregarAlFinal(lista,nuevo_nodo);
+            }
+        }
+    }else{
+        lista=nuevo_nodo;
+    }
+    return lista;
+}
+nodoDoble* eliminarNodoDoble    (nodoDoble*lista,char nombre_borrar[]){
+    if(lista){
+        if(strcmpi(aux->cliente.nombre,nombre_borrar)==0){
+            nodoDoble*aBorrar=lista;
+            lista=lista->siguiente;
+            if(lista->siguiente){
+                    nodoDoble*aux=lista->siguiente;
+                aux->anterior=lista;
+            }
+            free(aBorrar);
+        }else{
+            nodoDoble*ante=lista;
+            nodoDoble*seg=lista->siguiente;
+            while(seg && strcmpi(aux->cliente.nombre,nombre_borrar)!=0 ){
+                ante=seg;
+                seg=seg->siguiente;
+            }
+            if(seg){
+                ante->siguiente=seg->siguiente;
+                nodoDoble*aux=seg->siguiente;
+                aux->anterior=ante;
+                free(seg);
+            }else{
+                printf("El dato a borrar no esta en la lista\n");
+            }
+        }
+    }
+    return lista;
+}
+void mostrarNodo                (Cliente aux){
+    mostrarCliente(aux);
+}
+void mostrarListaDoble          (nodoDoble* lista){
+    if(lista){
+        nodoDoble*aux;
+        while(aux){
+            mostrarNodo(aux);
+            aux=aux->siguiente;
+        }
+    }
+}
 ///PROTOTIPADO TDA FILA
